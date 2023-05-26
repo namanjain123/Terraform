539e74f0fc2eb11b6d6077d6d124e9f94f1dd0b7
namanjain123 <naman.nishi@gmail.com>
2023-05-27 01:38:53 +0530

    addition of the private apim

---
+++ Azure Api Management/sample_apim_private.tf
@@ -0,0 +1,43 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_api_management" "example" {
+  name                = "example-apim"
+  location            = "eastus"
+  resource_group_name = "example-resource-group"
+
+  sku_name = "Developer_1"
+
+  publisher_name       = "namanjain"
+  publisher_email      = "namanjain@example.com"
+  notification_sender_email = "notification-email@example.com"
+
+  tags = {
+    environment = "production"
+  }
+
+  virtual_network_configuration {
+    subnet_id = azurerm_subnet.example.id
+    private_ip_address_allocation = "Static"  # Set IP allocation to static
+    private_ip_address_version    = "IPv4"    # Use IPv4 address version
+  }
+
+  custom_hostname_configuration {
+    hostname = "api.example.com"
+    certificate {
+      thumbprint          = "1234567890"
+      thumbprint_algorithm = "SHA-1"
+    }
+  }
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "example-subnet"
+  resource_group_name  = "example-resource-group"
+  virtual_network_name = "myTFVnet"
+  address_prefixes     = ["10.0.1.0/24"]
+
+  enforce_private_link_endpoint_network_policies = true  # Enforce network policies
+  enforce_private_link_service_network_policies  = true  # Enforce network policies
+}
---
+++ Azure Api Management/sample_apim_withapi_policyofinbound.tf
@@ -0,0 +1,23 @@
+data "azurerm_api_management_api" "example" {
+  name                = "naman"
+  api_management_name = "example-apim"
+  resource_group_name = "example-resource-group"
+  revision            = "2"
+}
+
+resource "azurerm_api_management_api_policy" "example" {
+  api_name            = naman
+  api_management_name = example-apim
+  resource_group_name = example-resource-group
+
+  xml_content = <<XML
+<policies>
+  <inbound>
+    <validate-content unspecified-content-type-action="prevent" max-size="102400" size-exceeded-action="prevent" errors-variable-name="requestBodyValidation">
+            <content-type-map missing-content-type-value="application/json" />
+            <content type="application/json" validate-as="json" action="detect" schema-id="Id" allow-additional-properties="false" />
+        </validate-content>
+  </inbound>
+</policies>
+XML
+}
