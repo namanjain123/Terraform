a65804735bbdaed10008b42dfd22de442c66ffdb
namanjain123 <naman.nishi@gmail.com>
2023-06-01 00:13:06 +0530

    application gateway

---
+++ Azure Application Gateway/basic_gateway.tf
@@ -0,0 +1,53 @@
+# Create an Azure Application Gateway
+resource "azurerm_application_gateway" "example" {
+  name                = "example-appgateway"
+  resource_group_name = "resource-group"
+  location            = "westus"
+  sku {
+    name     = "Standard_v2"
+    tier     = "Standard_v2"
+    capacity = 2
+  }
+
+  gateway_ip_configuration {
+    name      = "ip-configuration"
+    subnet_id = "subnet"
+  }
+
+  frontend_port {
+    name = "frontend-port"
+    port = 80
+  }
+
+  frontend_ip_configuration {
+    name                 = "frontend-ip"
+    public_ip_address_id = "sampleip"
+  }
+
+  backend_address_pool {
+    name = "backend-pool"
+  }
+
+  backend_http_settings {
+    name                  = "backend-http-settings"
+    cookie_based_affinity = "Disabled"
+    port                  = 80
+    protocol              = "Http"
+    request_timeout       = 60
+  }
+
+  http_listener {
+    name                           = "http-listener"
+    frontend_ip_configuration_name = "frontend-ip"
+    frontend_port_name             = "frontend-port"
+    protocol                       = "Http"
+  }
+
+  request_routing_rule {
+    name                       = "example-routing-rule"
+    rule_type                  = "Basic"
+    http_listener_name         = "http-listener"
+    backend_address_pool_name  = "backend-pool"
+    backend_http_settings_name = "backend-http-settings"
+  }
+}
